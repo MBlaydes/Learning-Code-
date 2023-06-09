075e952346bde2d3e407cd46e8687f09e3a9ed51
MBlaydes <133038898+MBlaydes@users.noreply.github.com>
2023-06-09 18:56:56 +0100

    Update and rename HideDigitsChallenge.ipynb to HideDigitsChallenge.py

---
+++ HideDigitsChallenge.py
@@ -0,0 +1,6 @@
+# ask user for credit card number
+n = input("Please type your 16 digit card number ")
+
+# grab last four digits and print 12 X's with the 4 digits afterwards
+print("XXXXXXXXXXXX" + n[12:16])
+
